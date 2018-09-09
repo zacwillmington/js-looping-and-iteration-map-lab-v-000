137ccdedeb8bf2b157e8576341ac3bc79d2ca7e9
Zac Willmington <zac.willmington@gmail.com>
2018-09-09 16:36:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 // Code your solution in this file.
- 
+
+function lowerCaseDrivers (drivers) {
+    const reformattedNames = drivers.map(driver => driver.toLowerCase());
+    return reformattedNames;
+}
