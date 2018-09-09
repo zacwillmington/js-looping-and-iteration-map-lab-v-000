943e5a3b998298e8f7d69407d11bccfe6c771944
Zac Willmington <zac.willmington@gmail.com>
2018-09-09 16:47:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ function lowerCaseDrivers (drivers) {
     const reformattedNames = drivers.map(driver => driver.toLowerCase());
     return reformattedNames;
 }
+
+function nameToAttributes (drivers) {
+    const newDrivers = drivers.map(driver => Object.assigin({}, firstName: splitName(nameStr)[0]), lastName: splitName(nameStr)[1])
+    return newDrivers
+}
+
+
+function splitName(nameStr) {
+    let fullName = driver.split(" ");
+    return fullName;
+}
